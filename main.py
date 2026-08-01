a021d279b5597016bc839363611d4d86c795f91b
Ola <...>
2026-08-01 10:46:42 +0200

    Drobne poprawki w formatowaniu kodu z użyciem Black Formatter, usunięnie niepotrzebnych print() do sprawdzania w terminalu wartości, poprawa literówek w komentarzach, dodanie brakujących ":" w kodzie zapisującym do pliku wynikowego, zapis całej treści pliku wynikowego w 1 linijce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-# stałą zawierająca ścieżkę do pliku txt
-TXT_FILE = r'server_logs.txt'
+
+# stała zawierająca ścieżkę do pliku txt
+TXT_FILE = r"server_logs.txt"
 
 # pusta lista na zapytania zakończone sukcesem
 successful = []
@@ -10,19 +11,16 @@ errors = []
 
 # otwarcie pliku i wyświetlenie go w konsoli
 try:
-    with open(TXT_FILE, 'r', newline='', encoding='utf-8') as file:
+    with open(TXT_FILE, "r", newline="", encoding="utf-8") as file:
         content = file.readlines()
-        for line in content:
-            print(line)
 
         # Ile było zapytań?
         total_lines = len(content)
-        print(total_lines)
 
         # Ile zapytań zakończyło się sukcesem, a ile błędem?
-        # - otrzymanie od razu kodu statusu, a pominięcie pozostałych kolumn
+        # - otrzymanie od razu kodu statusu z pominięciem pozostałych kolumn
         for line in content:
-            status_code = int(line.strip('\n').split()[-1])
+            status_code = int(line.strip("\n").split()[-1])
             if status_code > 400:
                 errors.append(status_code)
             elif 200 <= status_code < 300:
@@ -30,11 +28,9 @@ try:
 
         # zapisanie udanych zapytań do zmiennej
         total_successful = len(successful)
-        print(total_successful)
 
         # zapisanie nieudanych zapytań do zmiennej
         total_errors = len(errors)
-        print(total_errors)
 except FileNotFoundError:
     print("Nie znaleziono pliku.")
     sys.exit()
@@ -42,12 +38,10 @@ except FileNotFoundError:
 # zapis wyników do pliku analysis_results.txt
 
 try:
-    with open('analysis_results.txt', 'w+', newline='\n', encoding='utf-8') as file:
-        file.write('Wyniki analizy:\n--------------\n')
-        file.write('')
-        file.write(f'Liczba zapytań: {total_lines}\n')
-        file.write(f'Liczba udanych zapytań {total_successful}\n')
-        file.write(f'Liczba nieudanych zapytań {total_errors}\n')
+    with open("analysis_results.txt", "w+", newline="\n", encoding="utf-8") as file:
+        file.write(
+            f"Wyniki analizy:\n--------------\nLiczba zapytań: {total_lines}\nLiczba udanych zapytań: {total_successful}\nLiczba nieudanych zapytań: {total_errors}\n"
+        )
 except NameError:
     print("Brak danych.")
     sys.exit()
